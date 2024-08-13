289bc23ddd312029d2e7952aae711cf2b11eacce
Lenin <sclenins@gmail.com>
2024-08-12 19:38:47 -0500

    Primer commit de la clase

---
+++ 08-12 Funciones/speed/index.js
@@ -0,0 +1,10 @@
+function calcularVelocidad() {
+  const distancia = 10;
+  const tiempo = 2;
+  const velocidad = distancia / tiempo;
+  console.log("La velocidad del movil es " + velocidad);
+}
+
+calcularVelocidad();
+calcularVelocidad();
+calcularVelocidad();
