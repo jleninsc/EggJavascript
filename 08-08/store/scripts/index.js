3737bde7a5070c6da8ebbeb238d5f32f0cbf22bf
Lenin <sclenins@gmail.com>
2024-08-12 20:33:45 -0500

    Se modificó la lógica de index.js

--- 08-08/store/scripts/index.js
+++ 08-08/store/scripts/index.js
@@ -1,16 +1,34 @@
-const comprar = () => {
+const comprar = (nameClient) => {
   let totalApagar = 0;
+  alert("Hola " + nameClient + ", te guiaré en tu proceso de compra.");
+  console.log("Productos comprados por " + nameClient + ":");
   for (let index = 1; index <= 3; index++) {
-    const nombre = prompt("¿Qué producto desea llevar?");
-    const cantidad = Number(prompt("¿Cuántas unidades?"));
-    const precio = Number(prompt("¿Cuánto sale cada unidad?"));
+    const product = prompt("¿Qué producto deseas llevar?");
+    const cantidad = Number(
+      prompt("¿Cuántas unidades de " + product + " necesitas?")
+    );
+    const precio = Number(prompt("¿A cuánto te vendieron la unidad?"));
+
     const subtotal = cantidad * precio;
     totalApagar = totalApagar + subtotal;
+
+    console.log(
+      index +
+        ".- " +
+        product +
+        "\t" +
+        cantidad +
+        " unds. \tS/. " +
+        precio +
+        ".00 c/u\tS/. " +
+        subtotal +
+        ".00"
+    );
   }
-  console.log("Parcial cliente: "+totalApagar);
+  console.log("Total Parcial: S/. " + totalApagar + ".00");
   return totalApagar;
 };
 
-const total = comprar() + comprar() + comprar();
+const total = comprar("Juan") + comprar("Maria") + comprar("Carlos");
 
-console.log("El total registrado en caja fue de S/. " + total +".00")
+console.log("El total registrado en caja fue de S/. " + total + ".00");
