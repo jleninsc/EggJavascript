1a08dc5b6932e3a46c7d519f9a28ce55c3993258
Lenin <sclenins@gmail.com>
2024-08-12 19:57:31 -0500

    Cuarto commit de la clase

--- 08-12 Funciones/speed/index.js
+++ 08-12 Funciones/speed/index.js
@@ -2,10 +2,14 @@ const calcularVelocidad = (distancia, tiempo) => {
   return distancia / tiempo;
 };
 
-const velocidad1 = calcularVelocidad(10,2);
-const velocidad2 = calcularVelocidad(30,3);
-const velocidad3 = calcularVelocidad(48,2);
-console.log("La velocidad de Juana es " + velocidad1);
-console.log("La velocidad de Pedro es " + velocidad2);
-console.log("La velocidad de Maria es " + velocidad3);
-
+const velocidad1 = calcularVelocidad(10, 2);
+const velocidad2 = calcularVelocidad(30, 3);
+const velocidad3 = calcularVelocidad(48, 2);
+console.log(
+  "Las velocidades resultantes son:\nPedro\t\t: " +
+    velocidad1 +
+    " m/s\nMaria\t\t: " +
+    velocidad2 +
+    " m/s\nMiguel\t\t: " +
+    velocidad3 +" m/s"
+);
