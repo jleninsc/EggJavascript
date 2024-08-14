81dabf16776c1d79b09b31b51f158dfd58b171c4
Lenin <sclenins@gmail.com>
2024-08-13 20:43:23 -0500

    Avance de clase 2da actividad

--- 08-13 POO/scripts/product.js
+++ 08-13 POO/scripts/product.js
@@ -1,12 +1,30 @@
-const producto1 = {
-  nombre: "celular",
-  precio: 100000,
-  stock: 2,
-};
+class Product {
+  constructor(id, title, price, stock, images, onsale, supplier) {
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.stock = stock;
+    this.images = images;
+    this.onsale = onsale;
+    this._supplier = supplier;
+  }
+  get getSupplier() {
+    return this._supplier;
+  }
 
-producto1.id = "id123";
-producto1["foto"] = "https://i.postimg.cc/Jn2C5W84/galaxy1.webp";
+  set setSupplier(newName) {
+    this._supplier = newName;
+  }
+}
 
+const prod1 = new Product();
+const prod2 = new Product("id1", "title1", 3);
+const prod3 = new Product("id2", "title2", 4, 3, "images2", 5);
+const prod4 = new Product();
 
-console.log(producto1);
-console.log("El producto vendido fue: " + producto1.nombre);
+prod4.setSupplier = "Lenin";
+
+console.log(prod1);
+console.log(prod2);
+console.log(prod3);
+console.log(prod4.getSupplier);
